927016c01bf00bbd6f2950013c19b0d54d1945f9
Pedro Henrique Godri <pedro.g2006@aluno.ifsc.edu.br>
2023-06-29 19:48:14 -0300

    Camada de serviços

---
+++ api-front/src/app/model/Cliente.ts
@@ -0,0 +1,6 @@
+export class Cliente {
+  codigo:Number = 0;
+  nome:String = "";
+  idade:Number = 0;
+  cidade:String = "";
+}
---
+++ api-front/src/app/principal/principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.scss']
+})
+export class PrincipalComponent {
+  //Váriavel para visibilidade dos botões
+  btnCadastro:Boolean = true
+}
